a2424dfc4b9eee57619c0806fadf2a997e86be63
unknownguyoffline <unknownguy2912@gmail.com>
2025-02-18 02:36:01 +0000

    Added support for linux

---
+++ engine/platform/linux/LinuxWindow.cpp
@@ -0,0 +1,69 @@
+#include "LinuxWindow.h"
+#include <Macro.hpp>
+
+uint32_t WindowsWindow::mWindowCount = 0;
+
+Window *createWindow(const char *title, uint32_t width, uint32_t height)
+{
+    return new WindowsWindow(title, width, height);
+}
+
+const char *WindowsWindow::getTitle()
+{
+    return glfwGetWindowTitle(mWindow);
+}
+
+glm::uvec2 WindowsWindow::getSize()
+{
+    int width, height;
+    glfwGetWindowSize(mWindow, &width, &height);
+    return glm::uvec2(width, height);
+}
+
+glm::uvec2 WindowsWindow::getPosition()
+{
+    int x, y;
+    glfwGetWindowPos(mWindow, &x, &y);
+    return glm::uvec2(x, y);
+}
+
+bool WindowsWindow::isOpen()
+{
+    return !glfwWindowShouldClose(mWindow);
+}
+
+WindowsWindow::WindowsWindow(const char *title, uint32_t width, uint32_t height)
+{
+    if (mWindowCount == 0)
+    {
+        glfwInit();
+    }
+    mWindowCount++;
+
+    mWindow = glfwCreateWindow(width, height, title, nullptr, nullptr);
+
+    if (mWindow == nullptr)
+    {
+        CORE_ERROR("Failed to create window");
+    }
+
+    glfwMakeContextCurrent(mWindow);
+}
+
+void WindowsWindow::swapBuffer()
+{
+    glfwSwapBuffers(mWindow);
+}
+
+WindowsWindow::~WindowsWindow()
+{
+    glfwDestroyWindow(mWindow);
+    mWindowCount--;
+    if (mWindowCount == 0)
+        glfwTerminate();
+}
+
+void pollEvent()
+{
+    glfwPollEvents();
+}
--- project/testbed/testbed.cpp
+++ project/testbed/testbed.cpp
@@ -1,4 +1,5 @@
 #include <Engine.hpp>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <core/Macro.hpp>
 #include <glm/glm.hpp>
@@ -17,6 +18,7 @@ class App : public Gear::Application
 
     void update() override
     {
+        glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(1.f, 1.f, 1.f, 1.f);
     }
     void end() override
